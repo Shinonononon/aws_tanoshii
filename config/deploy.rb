5b6df9e35a14c27aa8a30f9681d2e013ab8798a6
Shinonononon <h1karu.sn0928@gmail.com>
2024-07-14 00:25:52 +0900

    add eginx and unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10"    # 1
 
 set :application, "task_app"    # 2
-set :repo_url, "git@github.com:Shinonononon/aws_tanoshii.git"    # 3
+set :repo_url, "https://github.com/Shinonononon/aws_tanoshii.git"    # 3
 set :linked_files, %w{config/secrets.yml}   # 4
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
 set :keep_releases, 5   # 6
